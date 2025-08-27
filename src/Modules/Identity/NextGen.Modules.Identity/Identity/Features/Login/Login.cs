45ffda3ea1f179bebe31457c2a652963ec622db4
s_kour <s_kour@RDP-235>
2025-08-27 17:40:43 +0330

    Update Validation

--- src/Modules/Identity/NextGen.Modules.Identity/Identity/Features/Login/Login.cs
+++ src/Modules/Identity/NextGen.Modules.Identity/Identity/Features/Login/Login.cs
@@ -20,8 +20,17 @@ internal class LoginValidator : AbstractValidator<Login>
 {
     public LoginValidator()
     {
-        RuleFor(x => x.UserNameOrEmail).NotEmpty().WithMessage("UserNameOrEmail cannot be empty");
-        RuleFor(x => x.Password).NotEmpty().WithMessage("password cannot be empty");
+        RuleFor(x => x.UserNameOrEmail)
+            .NotEmpty().WithMessage("UserNameOrEmail cannot be empty")
+            .MaximumLength(256).WithMessage("UserNameOrEmail cannot exceed 256 characters")
+            .When(x => x.UserNameOrEmail.Contains("@"))
+            .Matches(@"^[^@\s]+@[^@\s]+\.[^@\s]+$").WithMessage("Invalid email format");
+
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Password cannot be empty")
+            .MinimumLength(8).WithMessage("Password must be at least 8 characters")
+            .MaximumLength(128).WithMessage("Password cannot exceed 128 characters")
+            .Matches(@"[A-Za-z0-9!@#$%^&*]").WithMessage("Password must contain at least one letter, number, or special character");
     }
 }
 
